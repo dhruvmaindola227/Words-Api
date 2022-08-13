806cb094e26fe68b6fd18a43f448c1e0dab08da2
Dhruv <maindola.dhruv@gmail.com>
2022-08-13 17:21:19 +0530

    removed heroku changes

--- src/main/java/com/example/wordsapi/Controller/MainController.java
+++ src/main/java/com/example/wordsapi/Controller/MainController.java
@@ -11,7 +11,7 @@ import java.util.HashSet;
 
 
 @RestController
-@RequestMapping(path = "https://words-memory-game.herokuapp.com")
+@RequestMapping(path = "/wordsgame")
 
 public class MainController {
     @Autowired
